e4aa583a402d217c88b9d2b88ecdb7be9e720495
doantungbk <doantungbk.203@gmail.com>
2016-06-26 23:50:35 -0700

    Change the name

---
+++ tacker/vm/monitor_drivers/alarm_listener/alarm_listener.py
@@ -0,0 +1,102 @@
+#from keystoneclient.auth.identity import v2
+#from keystoneclient import session
+#from ceilometerclient import client
+
+
+#auth=v2.Password(auth_url="hosturl:5000/v2.0/", username="admin", password="pass", tenant_id="123456")
+#sess = session.Session(auth=auth, verify=False)
+#token=auth.get_token(sess)
+
+
+
+#cclient =client.get_client(2,ceilometer_url="hosturl:8777/",token=token,verify=False)
+#cclient.alarms.create()
+
+# Requirement for AMOD here:
+# Something related to alarm should be expressed here:
+# two ways: 1 - call alarm from Heat API             2 - call alarm from Ceilometer API
+# If alarms trigger then log
+
+
+from oslo_config import cfg
+from tacker.common import log
+from tacker.openstack.common import log as logging
+from tacker.vm.monitor_drivers import abstract_driver
+from tacker.i18n import _LW
+
+
+LOG = logging.getLogger(__name__)
+OPTS = [
+    cfg.StrOpt('period', default='60',
+               help=_('period time)),
+    cfg.StrOpt('evaluation_period', default='1',
+               help=_('evaluation period')),
+    cfg.StrOpt('threshold', default='70',
+               help=_('threshold'))
+]
+cfg.CONF.register_opts(OPTS, 'monitor_amod')
+
+class VNFMonitoringAlarm(abstract_driver.VNFMonitorAbstractDriver):  # Here abstract_driver is used
+
+    def get_type(self):
+        return 'alarm_listener'
+    def get_name(self):
+        return 'alarm_listener'
+    def get_description(self):
+        return 'Tacker alarm-based monitoring driver for VNF'
+    def monitor_url(self, plugin, context, device):
+        LOG.debug(_('monitor_url %s'), device)
+        return device.get('monitor_url', '')
+    @log.log
+    def monitor_call(self, device, kwargs):
+
+# Create new webhook here?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
